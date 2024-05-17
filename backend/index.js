dff7772b0ba775a31f85dc53dcc07ca8e8eab853
Tushar <123140222+tusharXO@users.noreply.github.com>
2024-05-17 15:07:12 +0530

    added mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,35 @@
 import express, { request, response } from "express";
 import { PORT, mongoDBRUL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Welcome to MERN Stack Tutorial')
+});
+
+app.post('/books', async (request, response) => {
+    try {
+        if(!request.body.title || !request.body.author || !request.body.publishYear){
+            return response.status(400).send({message: 'Send all required fields: title, author, publishYear'})
+        };
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error);
+        response.status(500).send({message: error.message});
+    }
 });
 
 mongoose
