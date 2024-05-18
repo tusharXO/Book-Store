4c92f2a6a3b1aa39f6cb7afbc7a2dfcac9017c97
Tushar <123140222+tusharXO@users.noreply.github.com>
2024-05-18 20:49:17 +0530

    updated createBook.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import CreateBooks from './pages/CreateBooks'
 import DeleteBooks from './pages/DeleteBooks'
-import EditBook from './pages/EditBook'
 import ShowBooks from './pages/ShowBooks'
+import EditBooks from './pages/EditBooks'
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
       <Route path='/' element={<Home />} />
       <Route path='/books/create' element={<CreateBooks />} />
       <Route path='/books/details/:id' element={<ShowBooks />} />
-      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/edit/:id' element={<EditBooks />} />
       <Route path='/books/delete/:id' element={<DeleteBooks />} />
     </Routes>
   )
--- frontend/src/pages/EditBooks.jsx
+++ frontend/src/pages/EditBooks.jsx
@@ -1,17 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import BackButton from '../components/BackButton'
 import Spinner from '../components/Spinner'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 
-const CreateBooks = () =>{
+const EditBooks = () =>{
 
   const[title, setTitle] = useState('')
   const[author, setAuthor] = useState('')
   const[publishYear, setPublishYear] = useState('')
   const[loading, setLoading] = useState(false)
   const navigation = useNavigate()
+  const { id } = useParams()
+  useEffect(() => {
+    setLoading(true)
+    axios.get(`http://localhost:5555/books/${id}`)
+    .then((response) => {
+      setAuthor(response.data.author)
+      setPublishYear(response.data.publishYear)
+      setTitle(response.data.title)
+      setLoading(false)
+    }).catch((error) => {
+      setLoading(false);
+      console.log(error)
+    })
+  }, [])
 
   const handSaveBook = () => {
     const data = {
@@ -32,7 +46,7 @@ const CreateBooks = () =>{
   return (
     <div className='p-4'>
       <BackButton />
-      <h1 className='text-3xl my-4'>Create Book</h1>
+      <h1 className='text-3xl my-4'>Edit Book</h1>
       {
         loading ? <Spinner /> : ""}
           <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
@@ -71,4 +85,4 @@ const CreateBooks = () =>{
   )
 }
 
-export default CreateBooks
+export default EditBooks
