ea26d71a9e540213768065e645b6606b09cdabf4
Tushar <123140222+tusharXO@users.noreply.github.com>
2024-05-18 12:52:11 +0530

    completed show book operation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -57,7 +57,10 @@ const Home = () => {
                   <BsInfoCircle className='text-2xl text-green-800' />
                 </Link>
                 <Link to={`/books/edit/${books._id}`}>
-                
+                  <AiOutlineEdit className='text-2xl text-yellow-600' />
+                </Link>
+                <Link to={`/books/delete/${book._id}`}>
+                  <MdOutlineDelete className='text-2xl text-red-600' />
                 </Link> 
                 </div></td>
             </tr>
