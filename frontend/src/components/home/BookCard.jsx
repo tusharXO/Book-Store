effa9cb5e9111d10649b17b667dfe98734828bf8
Tushar <123140222+tusharXO@users.noreply.github.com>
2024-05-20 18:55:14 +0530

    did the final changes  and now it sworking

--- frontend/src/components/home/BookCard.jsx
+++ frontend/src/components/home/BookCard.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import { PiBookOpenTextLight } from 'react-icons/pi'
-import { BiUserCircle } from 'react-icons/ai'
+import { BiUserCircle } from 'react-icons/bi'
 import { AiOutlineEdit } from "react-icons/ai"
 import { BsInfoCircle } from "react-icons/bs"
 import { MdOutlineDelete } from "react-icons/md"
@@ -8,7 +8,7 @@ import { MdOutlineDelete } from "react-icons/md"
 const BookCard = ({ books }) => {
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {books.map((item) => {
+        {books.map((item) => (
             <div key={item._id}
             className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl"
             >
@@ -38,7 +38,7 @@ const BookCard = ({ books }) => {
                     </Link>
                 </div>
             </div>
-        })}
+        ))}
     </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,6 +13,7 @@ const Home = () => {
 
     const [books, setBooks] = useState([]);
     const [loading, setLoading] = useState([]);
+    const [showType, setShowType] = useState('table');
 
     useEffect(() => {
         setLoading(true);
@@ -28,6 +29,14 @@ const Home = () => {
 
   return (
     <div className='p-4'>
+      <div className='flex justify-center items-center gap-x-4'>
+        <button className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg' onClick={() => {setShowType('table')}}>
+          Table
+        </button>
+        <button className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg' onClick={() => {setShowType('card')}}>
+          Card
+        </button>
+      </div>
       <div className='flex justify-between items-center'>
         <h1 className='text-3xl my-8'>Books List</h1>
         <Link to='/books/create'>
@@ -36,8 +45,8 @@ const Home = () => {
       </div>
       {loading ? 
       (<Spinner />) : 
-      (
-       <BookCard books={books} />
+      ( showType === "table" ?
+       (<BookTable books={books} />) : (<BookCard books={books} />)
       )}
     </div>
   )
